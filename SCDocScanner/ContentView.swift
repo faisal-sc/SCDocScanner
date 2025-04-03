061f337ebc564afdd72e5af320a76bb7de86e953
Zahidur Rahman Faisal <faisal.rahman@safetyculture.io>
2025-04-04 00:10:21 +1100

    Initial commit

--- SCDocScanner/ContentView.swift
+++ SCDocScanner/ContentView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  
+  @State private var scannedImages: [UIImage] = []
+      @State private var isShowingSCDocumentScannerView = false
+
+      var body: some View {
+          NavigationView {
+              Grid {
+                  ForEach(scannedImages, id: \.self) { image in
+                      VStack {
+                          Image(uiImage: image)
+                              .resizable()
+                              .scaledToFit()
+                              .frame(width: 100, height: 100)
+                              .padding()
+                      }
+                  }
+              }
+              .sheet(isPresented: $isShowingSCDocumentScannerView) {
+                SCDocumentScannerViewControllerRepresentable(scanResult: $scannedImages)
+              }
+              .toolbar {
+                  ToolbarItem {
+                      Button(action: showVNDocumentCameraView) {
+                        Text("Scan Document")
+                      }
+                  }
+              }
+          }
+      }
+
+      private func showVNDocumentCameraView() {
+          isShowingSCDocumentScannerView = true
+      }
 }
 
 #Preview {
---
+++ SCDocScanner/SCDocumentScannerViewControllerRepresentable.swift
@@ -0,0 +1,54 @@
+//
+//  SCDocumentScannerViewControllerRepresentable.swift
+//  SCDocScanner
+//
+//  Created by Faisal-SC on 3/4/2025.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+import VisionKit
+
+struct SCDocumentScannerViewControllerRepresentable: UIViewControllerRepresentable {
+  
+    @Binding var scanResult: [UIImage]
+
+  func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
+          let documentCameraViewController = VNDocumentCameraViewController()
+          documentCameraViewController.delegate = context.coordinator
+
+          return documentCameraViewController
+      }
+
+      func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) { }
+
+      func makeCoordinator() -> Coordinator {
+          Coordinator(scanResult: $scanResult)
+      }
+
+      final class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+          @Binding var scanResult: [UIImage]
+
+          init(scanResult: Binding<[UIImage]>) {
+              _scanResult = scanResult
+          }
+
+          /// Tells the delegate that the user successfully saved a scanned document from the document camera.
+          func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+              controller.dismiss(animated: true, completion: nil)
+              scanResult = (0..<scan.pageCount).compactMap { scan.imageOfPage(at: $0) }
+          }
+
+          // Tells the delegate that the user canceled out of the document scanner camera.
+          func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+              controller.dismiss(animated: true, completion: nil)
+          }
+
+          /// Tells the delegate that document scanning failed while the camera view controller was active.
+          func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+              print("Document scanner error: \(error.localizedDescription)")
+              controller.dismiss(animated: true, completion: nil)
+          }
+      }
+}
