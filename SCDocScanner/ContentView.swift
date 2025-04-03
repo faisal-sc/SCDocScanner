77c542ad0ebd08d50444de0463b03e220080e3d8
Zahidur Rahman Faisal <faisal.rahman@safetyculture.io>
2025-04-03 23:14:32 +1100

    Initial Commit

---
+++ SCDocScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SCDocScanner
+//
+//  Created by Faisal-SC on 3/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SCDocScanner/SCDocScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCDocScannerApp.swift
+//  SCDocScanner
+//
+//  Created by Faisal-SC on 3/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct SCDocScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
